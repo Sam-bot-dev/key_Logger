1a652a5e4c79b5c621edb34b2bb094fe09c5c48d
Sammy <bavesh2006kumar@gmail.com>
2024-09-19 23:29:24 +0530

    install packages

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 # I have successfull created and updated github repositry with vscode 
+# The package pynput.keyboard contains classes for controlling and monitoring the keyboard.
+import pynput
+from pynput.keyboard import Key,Listener
